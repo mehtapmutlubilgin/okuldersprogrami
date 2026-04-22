faf536e9eb1f4d13b199abf19437316fb43ff62b
Mehtap MUTLU BİLGİN <mehtap.mutlu.bilgin06@gmail.com>
2026-04-22 10:34:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-st.markdown("<h1 class='stTitle'>📅 Akıllı Ders Programı Asistanı</h1>", unsafe_allow_html=True)
+st.markdown("<h1 class='stTitle'>📅 OKUL ARKADAŞIM</h1>", unsafe_allow_html=True)
 
 # --- ÖRNEK SORULAR PANALİ ---
 st.markdown("""
